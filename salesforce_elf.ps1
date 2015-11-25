368afdb0095f688bd5fc072eccff9b37ae5ac745
midnightfreddie <jim@midnightfreddie.com>
2015-11-24 20:36:10 -0600

    Checkpoint. Getting close to done.

--- salesforce_elf.ps1
+++ salesforce_elf.ps1
@@ -35,14 +35,12 @@
  #* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  #*/
 
-# Not ready yet. abort! abort!
-break
-
-param(
-    $UserName,
-    $Password,
-    $Instance,
-    $Day
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]$UserName,
+    [Parameter(Mandatory=$True)]$Password,
+    [Parameter(Mandatory=$True)]$Instance,
+    [Parameter(Mandatory=$True)]$Day
 )
 
 ####### The following prompts replaced by parameters. Left here in comments for documentation.
@@ -88,22 +86,28 @@ $Parameters = @{
 }
 $Elfs = Invoke-RestMethod @Parameters
 
-#set the three variables to the array of Ids, EventTypes, and LogDates which will be used when downloading the files into your directory
-ids=( $(echo ${elfs} | ./jq -r ".records[].Id" | sed 's/[ \t]*$//') )
-eventTypes=( $(echo ${elfs} | ./jq -r ".records[].EventType" | sed 's/[ \t]*$//') )
-logDates=( $(echo ${elfs} | ./jq -r ".records[].LogDate" | sed 's/'T.*'//' | sed 's/[ \t]*$//') )
+##### Wow, forget all this crap. We'll ForEach_Object the results
+# 
+# #set the three variables to the array of Ids, EventTypes, and LogDates which will be used when downloading the files into your directory
+# ids=( $(echo ${elfs} | ./jq -r ".records[].Id" | sed 's/[ \t]*$//') )
+# eventTypes=( $(echo ${elfs} | ./jq -r ".records[].EventType" | sed 's/[ \t]*$//') )
+# logDates=( $(echo ${elfs} | ./jq -r ".records[].LogDate" | sed 's/'T.*'//' | sed 's/[ \t]*$//') )
+# 
+# #loop through the array of results and download each file with the following naming convention: EventType-LogDate.csv
+# for i in "${!ids[@]}"; do
+#     
+#     #uncomment the next three lines if you want to see the array of Ids, EventTypes, and LogDates
+#     echo "${i}: ${ids[$i]}"
+#     echo "${i}: ${eventTypes[$i]}"
+#     echo "${i}: ${logDates[$i]}"
+# 
+#     #make directory to store the files by date
+#     mkdir "${logDates[$i]}"
+# 
+#     #download files into the logDate directory
+#     curl --compressed "https://na1.salesforce.com/services/data/v32.0/sobjects/EventLogFile/${ids[$i]}/LogFile" -H 'Authorization: Bearer {AccessToken}' -H "X-PrettyPrint:1" -o "${logDates[$i]}/${eventTypes[$i]}.csv"
+# done 
 
-#loop through the array of results and download each file with the following naming convention: EventType-LogDate.csv
-for i in "${!ids[@]}"; do
-    
-    #uncomment the next three lines if you want to see the array of Ids, EventTypes, and LogDates
-    echo "${i}: ${ids[$i]}"
-    echo "${i}: ${eventTypes[$i]}"
-    echo "${i}: ${logDates[$i]}"
-
-    #make directory to store the files by date
-    mkdir "${logDates[$i]}"
-
-    #download files into the logDate directory
-    curl --compressed "https://na1.salesforce.com/services/data/v32.0/sobjects/EventLogFile/${ids[$i]}/LogFile" -H 'Authorization: Bearer {AccessToken}' -H "X-PrettyPrint:1" -o "${logDates[$i]}/${eventTypes[$i]}.csv"
-done 
+$Elfs | ForEach-Object {
+    Write-Verbose "$($_.Id), $($_.EventType), $($_.LogDate)"
+}
