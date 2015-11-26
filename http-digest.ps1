6d7358b7d5f36a100e82321febe1bbc349545a7e
midnightfreddie <jim@midnightfreddie.com>
2015-11-26 00:26:50 -0600

    Comments

--- http-digest.ps1
+++ http-digest.ps1
@@ -1,6 +1,10 @@
 ﻿# 2015-11-25
-# https://www.reddit.com/r/PowerShell/comments/3u7p4j/is_it_possible_to_use_digest_authentication_with/
+# In reply to https://www.reddit.com/r/PowerShell/comments/3u7p4j/is_it_possible_to_use_digest_authentication_with/
+# Using as reference: https://en.wikipedia.org/wiki/Digest_access_authentication
 # NOT COMPLETE OR TESTED AGAINST DIGEST AUTH SERVER. This is just an example at how I think this would go.
+# TODO: Make suthorization into a function? Perhaps have it increase nc and recalculate each time it's called
+# TODO: Better yeat, wrap Invoke-Webrequest in function that increments nc and recalculates hashes
+# TODO: And this wrapped function could catch 401 replies and then authenticate automagically?
 
 [cmdletbinding()]
 param(
@@ -49,7 +53,6 @@ Digest username="$username",
 
 Write-Verbose $authorization
 
-# real code again, but presumed values
 # Splatting the Invoke-Webrequest parameters for readability/maintainability
 $Parameters = @{
     Uri = $Uri
