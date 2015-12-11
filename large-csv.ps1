cfb7b2c44e8de95874f9916671d349725285a264
midnightfreddie <jim@midnightfreddie.com>
2015-12-11 16:31:57 -0600

    Cleanup

--- large-csv.ps1
+++ large-csv.ps1
@@ -26,7 +26,7 @@ filter Track-Info {
         $_
     }
     end {
-        Write-Verbose "Ending working set: $((Get-Process -PID $pid).WorkingSet64)"
+        Write-Verbose "Rows: $RowCount, ending working set: $((Get-Process -PID $pid).WorkingSet64)"
 
     }
 }
@@ -37,37 +37,11 @@ Measure-Command {
         Export-Csv -NoTypeInformation $OutCsv
 }
 
-# For an 82MB CSV file
-# Days              : 0
-# Hours             : 0
-# Minutes           : 4
-# Seconds           : 44
-# Milliseconds      : 498
-# Ticks             : 2844989680
-# TotalDays         : 0.00329281212962963
-# TotalHours        : 0.0790274911111111
-# TotalMinutes      : 4.74164946666667
-# TotalSeconds      : 284.498968
-# TotalMilliseconds : 284498.968
-
 # Wait a few seconds to allow memory to free up
-Start-Sleep -Seconds 20
+Start-Sleep -Seconds 60
 
 Measure-Command {
     Get-Content $InCsv |
         Track-Info |
         Out-File -Encoding ascii $OutCsv
 }
-
-# For an 82MB CSV file
-# Days              : 0
-# Hours             : 0
-# Minutes           : 4
-# Seconds           : 59
-# Milliseconds      : 452
-# Ticks             : 2994525084
-# TotalDays         : 0.00346588551388889
-# TotalHours        : 0.0831812523333333
-# TotalMinutes      : 4.99087514
-# TotalSeconds      : 299.4525084
-# TotalMilliseconds : 299452.5084
