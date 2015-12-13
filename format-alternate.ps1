fab54d0e39df6297d796121f696b542ce4b66770
Jim Nelson <jim@midnightfreddie.com>
2015-12-13 00:50:03 -0600

    Changed to filter, manually incrementing row tracker

--- format-alternate.ps1
+++ format-alternate.ps1
@@ -1,23 +1,30 @@
 ﻿# From http://kunaludapi.blogspot.com/2015/12/format-table-color-style-odd-even-rows.html
 
-function Format-OddEven {
-    $input | foreach {
-        $Number = [array]::IndexOf($ProcessData, $_)
-        $ConsoleBack = [System.Console]::BackgroundColor
-        $ConsoleFore = [System.Console]::ForegroundColor
-        if (($Number % 2) -eq 0) {
-            [System.Console]::BackgroundColor = "DarkGray"
-            [System.Console]::ForegroundColor = "black"
-            $_ 
-        } #if
-        else {
+filter Format-OddEven {
+    begin {
+        $Number = 0
+    }
+    process {
+        $input | foreach {
+            $Number += 1
+            $ConsoleBack = [System.Console]::BackgroundColor
+            $ConsoleFore = [System.Console]::ForegroundColor
+            if (($Number % 2) -eq 0) {
+                [System.Console]::BackgroundColor = "DarkGray"
+                [System.Console]::ForegroundColor = "black"
+                $_ 
+            } #if
+            else {
+                [System.Console]::BackgroundColor = $ConsoleBack
+                [System.Console]::ForegroundColor = $ConsoleFore
+                $_ 
+            } #else
             [System.Console]::BackgroundColor = $ConsoleBack
             [System.Console]::ForegroundColor = $ConsoleFore
-            $_ 
-        } #else
-        [System.Console]::BackgroundColor = $ConsoleBack
-        [System.Console]::ForegroundColor = $ConsoleFore
-    } #input
+        } #input
+    }
+    end {
+    }
 <#
 .Synopsis
 Format table output row odd even colors.
