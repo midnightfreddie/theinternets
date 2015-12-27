66ed27ad883e27c0a154a4205230c1c20d3ee21f
Jim Nelson <jim@midnightfreddie.com>
2015-12-27 11:21:26 -0600

    My adjustments

--- angular-post.js
+++ angular-post.js
@@ -5,16 +5,21 @@ $scope.MakeGray_Button = function(){
         var MakeGray_Form = new FormData();
         MakeGray_Form.append("FileName", $scope.imageUrl);
         $http({
-        method : "POST",
-        url    : "../opencv/MakeGray/MakeGray.php",
-        data   : MakeGray_Form,
-        transformRequest: angular.identity,
-        headers: {'Content-Type': undefined}
-        }).
-        success(function(){
-           // some magic code here that grabs the $base64 variable from php
+          method : "POST",
+          url    : "../opencv/MakeGray/MakeGray.php",
+          data   : MakeGray_Form,
+          transformRequest: angular.identity,
+          headers: {'Content-Type': "application/x-www-form-urlencoded"}
         })
-        .error(function(){});
+          .then(function(result){
+              // code if success
+             var base64FromPhp = result.data;
+            },
+            function(result){
+              // code if error
+                console.log(result.status);
+            }
+          );
     }
     else{
         alert("Please upload an image");
