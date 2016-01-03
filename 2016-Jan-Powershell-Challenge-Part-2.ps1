2d67105213d8a716abc31b65647795df259139f4
Jim Nelson <jim@midnightfreddie.com>
2016-01-03 14:42:16 -0600

    Checkpoint for part 2...messed something up

---
+++ 2016-Jan-Powershell-Challenge-Part-2.ps1
@@ -0,0 +1,30 @@
+﻿# https://www.reddit.com/r/PowerShellChallenge/comments/3ywz6a/challenge_january_2016/
+# Part 2
+
+function Invoke-PowerCSV {
+    param (
+        $ComputerName,
+        $InputFile,
+        $UpdateValue
+    )
+    Import-Csv $InputFile |
+        Where-Object { $_.ComputerName -eq $ComputerName } |
+        ForEach-Object {
+            $Row = $_
+            $FirstValue, $NewValues =  @(
+                ( $Row.psobject.Properties |
+                    Where-Object { $_.Name -ne "ComputerName" } |
+                    Select-Object -ExpandProperty Value
+                ),
+                $UpdateValue
+            )
+
+            $NewValues
+            #1..$_.psobject.Properties.Length | ForEach-Object {
+                
+            #}
+        } #|
+        #Export-Csv -NoTypeInformation $InputFile
+}
+
+Invoke-PowerCSV -ComputerName "Mercury" -UpdateValue 999 -InputFile "C:\Users\jim.AD\SkyDrive\Documents\Powershell\theinternets\Challenge1.csv"
