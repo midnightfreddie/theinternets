62a3de088e998f12a0122802f8c0a63649b698c7
Jim Nelson <jim@midnightfreddie.com>
2016-01-03 14:13:53 -0600

    Added Powershell challenge

---
+++ 2016-Jan-Powershell-Challenge.ps1
@@ -0,0 +1,19 @@
+﻿# https://www.reddit.com/r/PowerShellChallenge/comments/3ywz6a/challenge_january_2016/
+
+function Invoke-PowerCSV {
+    param (
+        $ComputerName,
+        $InputFile
+    )
+    Import-Csv $InputFile |
+        Where-Object { $_.ComputerName -eq $ComputerName } |
+        ForEach-Object {
+            $_.psobject.Properties |
+                Where-Object { $_.Name -ne "ComputerName" } |
+                ForEach-Object {
+                    $_.Value
+                }
+        }
+}
+
+Invoke-PowerCSV -ComputerName "Mercury" -InputFile "C:\Users\jim.AD\SkyDrive\Documents\Powershell\theinternets\Challenge1.csv"
